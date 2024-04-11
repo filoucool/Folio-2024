39418c60194f31f59c5dc8e332bbe2ccc6752a11
Félix Chenette-Stewart <felix@nexumnet.com>
2024-04-10 22:40:48 -0400

    added first canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from '@react-three/drei'; // If you're okay with OrbitControls for now
+import { createRoot } from 'react-dom/client';
+import { PointerLockControls } from '@react-three/drei';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container">
+    <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="blue" position={[1, 10, 15]} />
+      <mesh>
+        <boxGeometry />
+        <meshStandardMaterial />
+      </mesh>
+    </Canvas>
     </div>
-  );
+  )
 }
+
+createRoot(document.getElementById('root')).render(<App />)
 
 export default App;
